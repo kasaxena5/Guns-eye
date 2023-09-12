c91f54339326132caf299215f2aaa6d60fa0c3e1
kasaxena5 <kasaxena@microsoft.com>
2023-09-12 17:45:06 +0530

    Added a 3rd person character controller

---
+++ Assets/Scripts/GamePlay/PlayerController.cs
@@ -0,0 +1,65 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(CharacterController), typeof(PlayerInput))]
+public class PlayerController : MonoBehaviour
+{
+    [Header("Configs")]
+    [SerializeField] private float playerSpeed = 2.0f;
+    [SerializeField] private float jumpHeight = 1.0f;
+    [SerializeField] private float gravityValue = -9.81f;
+    [SerializeField] private float rotationSpeed = 5f;
+    
+    // Required components and global variables
+    private CharacterController controller;
+    private PlayerInput playerInput;
+    private Transform cameraTransform;
+    private Vector3 playerVelocity;
+    private bool groundedPlayer;
+
+    // PlayerControl Actions
+    private InputAction moveAction;
+    private InputAction lookAction;
+    private InputAction jumpAction;
+
+
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        playerInput = GetComponent<PlayerInput>();
+        cameraTransform = Camera.main.transform;
+        moveAction = playerInput.actions["Move"];
+        lookAction = playerInput.actions["Look"];
+        jumpAction = playerInput.actions["Jump"];
+    }
+
+    void Update()
+    {
+        groundedPlayer = controller.isGrounded;
+        if (groundedPlayer && playerVelocity.y < 0)
+        {
+            playerVelocity.y = 0f;
+        }
+
+        Vector2 input = moveAction.ReadValue<Vector2>();
+        Vector3 move = new Vector3(input.x, 0, input.y);
+        move = move.x * cameraTransform.right.normalized + move.z * cameraTransform.forward.normalized;
+        move.y = 0;
+        controller.Move(move * Time.deltaTime * playerSpeed);
+
+
+        // Changes the height position of the player..
+        if (jumpAction.triggered && groundedPlayer)
+        {
+            playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+        }
+
+        playerVelocity.y += gravityValue * Time.deltaTime;
+        controller.Move(playerVelocity * Time.deltaTime);
+
+        // Rotate towards camera direction
+        float targetAngle = cameraTransform.eulerAngles.y;
+        Quaternion rotation = Quaternion.Euler(0, targetAngle, 0);
+        transform.rotation = Quaternion.Lerp(transform.rotation, rotation, rotationSpeed);
+    }
+}
